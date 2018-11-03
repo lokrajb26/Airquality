aad7d4723256bf5acf2dbd05ffdb5d8d443d2372
lokrajb26 <lokrajb26@gmail.com>
2018-11-03 17:28:13 +0530

    first set of UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CustomPageComponent } from './custom-page/custom-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CustomPageComponent
   ],
   imports: [
     BrowserModule
